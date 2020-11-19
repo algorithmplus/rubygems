eab0e48d3f68725749c85b31bf0741b60f61e230
algorithmplus <algorithm_plus@yahoo.co.uk>
2020-11-19 16:05:13 +0000

    even more changes

---
+++ app/models/contract.rb
@@ -0,0 +1,4 @@
+class Contract < ApplicationRecord
+    validates :title,  presence: true
+    validates :description, presence: true, length: { :minimum => 5 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'users/new'
+  devise_for :models
+  resources :contracts
+  resources :courses
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
