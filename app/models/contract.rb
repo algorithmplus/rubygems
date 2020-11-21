2f6e09ea8cc8e05b43649536fe2758b7d40d5f5d
algorithmplus <algorithm_plus@yahoo.co.uk>
2020-11-21 17:06:50 +0000

    even more changes

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,7 +1,2 @@
 class Contract < ApplicationRecord
-    validates :title,  presence: true
-    validates :description, presence: true, length: { :minimum => 5 }
-    def to_s
-        title
-    end
 end
