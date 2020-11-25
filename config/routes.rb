567947057e666c9b24067299c04b4f3b446e962c
algorithmplus <algorithm_plus@yahoo.co.uk>
2020-11-25 16:24:03 +0000

    even more changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/new'
   resources :contracts
+  resources :users, only: [:index]
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
