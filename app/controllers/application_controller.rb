28c3b54fc7f1dbab064106e5f0f531914550c478
algorithmplus <algorithm_plus@yahoo.co.uk>
2020-12-06 14:32:16 +0000

    even more changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-      include Pundit
-      protect_from_forgery
-      rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
       before_action :authenticate_user!
+      protect_from_forgery
+      include Pundit
+      rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+          
+      
       before_action :set_global_variables, if: :user_signed_in?
-  def set_global_variables
-    @ransack_contracts = Contract.ransack(params[:contracts_search], search_key: :contracts_search) #navbar search
-  end
-end
+      
+      def set_global_variables
+         @ransack_contracts = Contract.ransack(params[:contracts_search], search_key: :contracts_search) #navbar search
+      end
+
 
  private
 
@@ -15,4 +18,4 @@ end
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
   end
-
+end
